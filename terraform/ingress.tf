3fa788a80f63bae490caa8330e0e83c8217c2567
Kurt Lehmer <kurtlehmer@Kurts-MacBook-Pro-2.local>
2024-11-12 21:33:10 -0500

    ingress alb health check

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -7,12 +7,13 @@ resource "kubernetes_ingress_v1" "block_xplorer_ingress" {
     name      = "block-xplorer-ingress"
     namespace = var.k8s_namespace
     annotations = {
-      "kubernetes.io/ingress.class"               = "alb"
-      "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
-      "alb.ingress.kubernetes.io/ssl-redirect"    = "443"
-      "alb.ingress.kubernetes.io/target-type"     = "instance"
-      "alb.ingress.kubernetes.io/certificate-arn" = module.acm.acm_certificate_arn
-      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTP\": 80}, {\"HTTPS\": 443}]"
+      "kubernetes.io/ingress.class"                = "alb"
+      "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+      "alb.ingress.kubernetes.io/ssl-redirect"     = "443"
+      "alb.ingress.kubernetes.io/target-type"      = "instance"
+      "alb.ingress.kubernetes.io/certificate-arn"  = module.acm.acm_certificate_arn
+      "alb.ingress.kubernetes.io/listen-ports"     = "[{\"HTTP\": 80}, {\"HTTPS\": 443}]"
+      "alb.ingress.kubernetes.io/healthcheck-path" = "/healthz"
     }
   }
 
