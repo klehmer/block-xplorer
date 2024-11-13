82e0f73742001cb149fbcb37eae963a64830e2a7
Kurt Lehmer <kurtlehmer@Kurts-MacBook-Pro-2.local>
2024-11-12 20:59:33 -0500

    add ecr lifecycle policy, delete unused output

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,8 +4,23 @@ resource "aws_ecr_repository" "block_xplorer" {
   image_tag_mutability = "MUTABLE" # Allow image tags to be mutable (optional)
 }
 
-# Output the repository URL
-output "ecr_repository_url" {
-  value       = aws_ecr_repository.block_xplorer.repository_url
-  description = "URL of the ECR repository for block-xplorer"
+resource "aws_ecr_lifecycle_policy" "block_xplorer_policy" {
+  repository = aws_ecr_repository.block_xplorer.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Retain only the 30 most recent images"
+        selection    = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 30
+        }
+        action       = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
